0b7c7408579b66d462b28e330edd21b866ae030b
Muhammad Enstein Widodo <48766167+menstein@users.noreply.github.com>
2022-09-12 21:51:32 +0700

    minor changes

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 timmy.shape("turtle")
+timmy.color("chartreuse2")
 
 myscreen = Screen()
-myscreen
+print(myscreen.canvheight)
+myscreen.exitonclick()
